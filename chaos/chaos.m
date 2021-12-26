da7c1307b3cc737e2fb66b674258dfab001fcf14
btfsoeon <btfsoeon@gmail.com>
2021-12-25 19:53:41 -0800

    chaos

---
+++ chaos/chaos.m
@@ -0,0 +1,47 @@
+function chaos(iter, initial)
+  
+  alpha = linspace(1.5, 4.0, 10);
+
+  for(i = 1:10)
+  
+    a = alpha(i);
+    r(i,1) = a;
+    curr = initial;
+    
+    for(j = 1:iter)
+      
+      prev = curr;
+      curr = a * prev * (1-prev);
+      
+      if (iter - j) < 25
+        % 1000-(1000-24) -> 2
+        % 1000-(1000-23) -> 3
+        % 1000-(1000-22) -> 4
+        % ...
+        % 1000-(1000-0) -> 26
+        r(i, 26-(iter-j)) = curr;
+      endif
+      
+    endfor
+    
+    %{
+    while(abs(p)>0.001 && j<=20)
+    
+      j=j+1
+      r(i,j)=a*r(i,j-1)*(1-r(i,j-1))
+      p=r(i,j)-r(i,j-1)
+      
+    endwhile
+    %}
+  
+  endfor
+
+  r
+  plot(alpha, r(:,2))  
+
+  %{
+  X=1:size(r)(2)
+  plot(X, r(1,:), "-", X, r(2,:), "-")
+  %}
+  
+endfunction
---
+++ chaos/chaos_n.m
@@ -0,0 +1,45 @@
+function chaos_n(iter, initialV, numAlphas)
+  
+  alphas=linspace(1.0,4.0,numAlphas);
+  
+  for k=1:numAlphas
+    
+    x(1,k)=initialV;
+    a=alphas(k);
+    
+    for i=1:iter
+      
+      x(i+1,k) = a * x(i,k) * (1-x(i,k));
+      
+    endfor
+    
+  endfor
+  
+  %plot(x(:,[2,4]),'*');
+  %xlabel('iteration');
+  %ylabel('value');
+  %xlim([0,iter]);
+  %legend('alpha=2','alpha=4');
+  %hold on
+  
+  % plot the solution vs. alpha
+  for j=1:numAlphas
+    
+    a=alphas(j)*ones(25);
+    y=x(iter-24:iter,j);
+    plot(a,y,'+');
+    hold on
+    
+  endfor
+  
+  x(:,5)
+  
+  %size(x)
+  #hold on
+  #figure(2)
+  #plot(x(:,2));
+  
+  #figure(3)
+  #plot(x(:,numAlphas));
+
+endfunction
